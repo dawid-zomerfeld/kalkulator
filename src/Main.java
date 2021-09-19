e5070aed8879a36d46761baabac3bfd295bbe862
Dawid Zomerfeld <dawid.zomerfeld@optibuy.com>
2021-09-19 19:00:41 +0200

    obliczanie wyrazenia z postaci ONP

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,64 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Wprowadź wyrażenie");
+        String expression = scan.nextLine();
+
+        System.out.println("Wprowadzono:  " + expression);
+        Integer calculate = calculate(expression);
+
+        System.out.println("Wynik:  " + calculate);
+
+    }
+
+
+
+
+    public static Integer calculate(String expression) {
+
+        String[] splitExpression = expression.trim().split(" ");
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (String s : splitExpression) {
+
+            if (s.matches("\\d+"))
+                stack.push(Integer.valueOf(s));
+
+            else {
+                Integer a;
+                Integer b;
+
+                switch (s) {
+                    case "+" -> {
+                        a = stack.pop();
+                        b = stack.pop();
+                        stack.push(b + a);
+                    }
+                    case "-" -> {
+                        a = stack.pop();
+                        b = stack.pop();
+                        stack.push(b - a);
+                    }
+                    case "*" -> {
+                        a = stack.pop();
+                        b = stack.pop();
+                        stack.push(b * a);
+                    }
+                    case "/" -> {
+                        a = stack.pop();
+                        b = stack.pop();
+                        stack.push(b / a);
+                    }
+                }
+            }
+
+        }
+
+        return stack.pop();
     }
 }
