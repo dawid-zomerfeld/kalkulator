da488cce6e8fb1258b315d286918fe3e52e0bdb5
Dawid Zomerfeld <dawid.zomerfeld@optibuy.com>
2021-10-06 21:11:25 +0200

    zamiana wyrażenia na ONP

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,73 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Wprowadź wyrażenie");
+        System.out.println("Wprowadź wyrażenie. Dozwolone znaki to : +, -, *, / ,(, ) oraz cyfry");
         String expression = scan.nextLine();
 
         System.out.println("Wprowadzono:  " + expression);
-        Integer calculate = calculate(expression);
+
+        String onp = convertToOnp(expression);
+
+        System.out.println("ONP:  " + onp);
+
+        Integer calculate = calculate(onp);
 
         System.out.println("Wynik:  " + calculate);
 
     }
 
 
+    public static String convertToOnp(String expression) {
 
+        Stack<Character> stack = new Stack<>();
+        StringBuilder outExpression = new StringBuilder();
+        Character actualChar;
+
+        for (int i = 0; i < expression.length(); i++) {
+
+            actualChar = expression.charAt(i);
+
+            // jeśli otwarty nawias
+            if (actualChar == '(') {
+                stack.push(actualChar);
+            }
+            // jeśli zamkniety nawias
+            if (actualChar == ')') {
+
+                while (!stack.isEmpty()) {
+                    Character stackCharacter = stack.pop();
+                    if (stackCharacter == '(')
+                        break;
+                    outExpression.append(" ").append(stackCharacter);
+                }
+            }
+
+            // jeśli cyfra to dajemy na wyjscie
+            if (Character.isDigit(actualChar)) {
+                outExpression.append(actualChar);
+            }
+            // jeśli jest operatorem
+            if (isOperator(actualChar)) {
+                outExpression.append(" ");
+                if (!stack.isEmpty()) {
+                    while (!stack.isEmpty()) {
+                        Character stackCharacter = stack.pop();
+                        if (priorityOfChar(actualChar) > priorityOfChar(stackCharacter)) {
+                            stack.push(stackCharacter);
+                            break;
+                        } else
+                            outExpression.append(" ").append(stackCharacter).append(" ");
+                    }
+                }
+                stack.push(actualChar);
+            }
+        }
+
+        while (!stack.isEmpty())
+            outExpression.append(" ").append(stack.pop());
+
+        return outExpression.toString().replaceAll("  ", " ");
+    }
 
     public static Integer calculate(String expression) {
 
@@ -60,5 +115,22 @@ public class Main {
         }
 
         return stack.pop();
+    }
+
+    private static boolean isOperator(Character operator) {
+
+        return "+-*/".contains(operator.toString());
+    }
+
+    private static int priorityOfChar(Character operator) {
+
+        int n = 0;
+
+        if (operator.equals('+') || operator.equals('-'))
+            n = 1;
+        if (operator.equals('*') || operator.equals('/'))
+            n = 2;
+
+        return n;
     }
 }
